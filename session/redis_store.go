bdb5fa5430aa010aec5798c299d0d8a04491252c
shenjiayu <sjy19930312@gmail.com>
2014-11-25 16:00:08 +0800

    format the code

--- session/redis_store.go
+++ session/redis_store.go
@@ -8,17 +8,6 @@ import (
 	"time"
 )
 
-type Store interface {
-	//initiate a new session (normally saved it into the cookie of users)
-	New(*http.Request, http.ResponseWriter) (*Session, error)
-	//save the session into backend (redis)
-	Save(*http.Request, http.ResponseWriter, *Cache) error
-	//get the session out of the backend (redis)
-	Get(*http.Request) (*Cache, error)
-	//purge the storage of session
-	Delete(http.ResponseWriter, *Cache) error
-}
-
 type RedisStore struct {
 }
 
